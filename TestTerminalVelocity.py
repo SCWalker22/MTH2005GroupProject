5de6948ed993fb101624b08059c0fe3bfb0d000d
SCWalker22 <stanthewalker1@gmail.com>
2026-03-10 14:18:36 +0000

    Cleaned up

--- TestTerminalVelocity.py
+++ TestTerminalVelocity.py
@@ -3,6 +3,9 @@ import matplotlib.pyplot as plt
 
 Rho_w = 1000.0
 pi = np.pi
+g = 9.81
+
+#Copy from here down
 
 def down_force(r: float) -> float:
     """
@@ -15,7 +18,6 @@ def down_force(r: float) -> float:
         Downwards force (N)
     """
     mass: float = Rho_w*pi*r**3
-    g = 9.81
     return mass*g
 
 def drag(
@@ -28,28 +30,6 @@ def drag(
     """
     area: float = pi*r**2
     return 0.5*rho*(v**2)*area*0.5
-
-# start_height = 500
-
-# dt = 0.01
-# end_t = 1
-# t = 0
-# v = 0
-# r = 1e-6
-# rho = 1.225
-# vels: list[float] = [v]
-# times: list[float] = [t]
-# while t < end_t:
-#     force = down_force(r) - drag(v, r, rho)
-#     mass: float = Rho_w*pi*r**3
-#     accel = force/mass
-#     v += dt*accel
-#     vels.append(v)
-#     t += dt
-#     times.append(t)
-
-# plt.plot(times, vels)
-# plt.show()
 
 def terminal_velocity(r: float, rho: float) -> float:
     dt = 0.01
@@ -64,5 +44,7 @@ def terminal_velocity(r: float, rho: float) -> float:
         t += dt
     return v
 
-print(terminal_velocity(1e-6, 1.225))
+# Stop copying here
+if __name__=="__main__":
+    print(terminal_velocity(1e-6, 1.225))
         
